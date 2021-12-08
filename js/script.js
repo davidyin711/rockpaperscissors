4a6bb6c32e5c463470f0220456347cf942e72162
Glitch (rock-paper-scissors-david-valentina) <none>
2021-12-08 23:19:07 +0000

    🌦🎁 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,5 +1,65 @@
-$(".play").click(function(){
+function handleWinner() {
+  $(".result").text("You win!");
+  $(".result").css("color", "green");
+}
+
+function handleTie() {
+  $(".result").text("It's a tie!");
+  $(".result").css("color", "orange");
+}
+
+function handleLoser() {
+  $(".result").text("You lose. Try again!");
+  $(".result").css("color", "red");
+}
+
+let globalNumber = 0;
+$(".play").click(function() {
+  let computerNumber;
+  computerNumber = Math.random() * 3;
+  computerNumber = Math.ceil(computerNumber);
+  // "computer choice"
+  $(".computerChoice").text(`computer choice is ${computerNumber}`);
+
   let userInput = $(".input").val();
+  userInput = userInput.toLowerCase();
+
   $(".userChoice").text(userInput);
-  // alert("hi");
+
+  if (computerNumber === 1) {
+    $(".computerChoice").text("Rock");
+  }
+  if (computerNumber === 2) {
+    $(".computerChoice").text("Scissors");
+  }
+  if (computerNumber === 3) {
+    $(".computerChoice").text("Paper");
+  }
+
+  if (computerNumber === 1 && userInput == "rock") {
+    handleTie();
+  } else if (computerNumber === 1 && userInput === "paper") {
+    handleWinner();
+  } else if (computerNumber === 1 && userInput === "scissors") {
+    handleLoser();
+  } else if (computerNumber === 2 && userInput === "scissors") {
+    handleTie();
+  } else if (computerNumber === 2 && userInput === "paper") {
+    handleLoser();
+  } else if (computerNumber === 2 && userInput === "rock") {
+    handleWinner();
+  } else if (computerNumber === 3 && userInput === "paper") {
+    handleTie();
+  } else if (computerNumber === 3 && userInput === "rock") {
+    handleLoser();
+  } else if (computerNumber === 3 && userInput === "scissors") {
+    handleWinner();
+  } else {
+    $(".result").text(
+      "oops... looks like there's a mistake in your input. Try checking the image to make sure you spelled your input correctly. And make sure to use capital letters!"
+    );
+  }
+
+  // this refreshes the input bar
+  $(".input").val("");
 });
